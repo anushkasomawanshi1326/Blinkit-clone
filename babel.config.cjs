2ba50253dc2f1d7f3cfe707d1c3d2b986dfeb18e
anushkasomawanshi1326 <anushkasomawanshi2612@gmail.com>
2025-12-23 10:26:59 +0530

    babel.config.cjs modified

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,19 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    ["@babel/preset-react", { runtime: "automatic" }]
-  ],
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current"
+        }
+      }
+    ],
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+        development: process.env.NODE_ENV === "test"
+      }
+    ]
+  ]
 };
